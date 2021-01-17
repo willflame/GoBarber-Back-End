2d214c5db208840629dedb3c48310cb8196559d6
Willian Anselmo <willian0506@hotmail.com>
2021-01-17 11:54:51 -0300

    Emails em desenvolvimento

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,15 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-// import IMailProvider from './MailProvider/models/IMailProvider';
-// import IMailProvider from './MailProvider/Implementations';
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/Implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
-// container.registerSingleton<IMailProvider>(
-//   'MailProvider',
-//   DiskStorageProvider,
-// );
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider(),
+);
