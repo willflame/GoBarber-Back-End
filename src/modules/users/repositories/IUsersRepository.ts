b1b4ec61bcec3486fb865c6c26b171804d8111e6
Willian Anselmo <willian0506@hotmail.com>
2021-02-22 22:33:59 -0300

    Listagem de prestadores

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
-import ICreateUserDto from '../dtos/ICreateUserDTO';
 import User from '../infra/typeorm/entities/User';
+import ICreateUserDto from '../dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '../dtos/IFindAllProvidersDTO';
 
 export default interface IUsersRepository {
+  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDto): Promise<User>;
