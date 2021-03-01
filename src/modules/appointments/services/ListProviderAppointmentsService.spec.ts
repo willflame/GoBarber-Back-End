69ed277102eb23cfd4582e5c37b9ac30e71a59f5
Willian Anselmo <willian0506@hotmail.com>
2021-03-01 20:55:45 -0300

    Agenda do prestador

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,39 @@
+import { createTrue } from 'typescript';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let ListProviderAppointment: ListProviderAppointmentsService;
+
+describe('ListProviderAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    ListProviderAppointment = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments an a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: '15165',
+      date: new Date(2021,4,20,8,0,0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: '151151',
+      date: new Date(2021,4,20,13,0,0),
+    });
+
+    const appointments = await ListProviderAppointment.execute({
+      provider_id: 'provider-id',
+      year: 2021,
+      month: 5,
+      day: 20,
+    });
+
+    expect(appointments).toEqual(expect.arrayContaining([appointment1,appointment2]));
+  });
+});
